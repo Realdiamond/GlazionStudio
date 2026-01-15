858bae02a788e6359f9518eb90208b14356ac7ea
Realdiamond <akinsanmioluwatimilehin@gmail.com>
2026-01-15 15:51:01 +0100

    2nd Update to the api folder

--- api/get-logins.ts
+++ api/get-logins.ts
@@ -4,3 +4,5 @@ export default async function handler(req, res) {
   const { rows } = await sql`SELECT * FROM login_events ORDER BY logged_in_at DESC LIMIT 100`;
   res.status(200).json(rows);
 }
+
+res.setHeader('Access-Control-Allow-Origin', '*');
